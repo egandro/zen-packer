aae2b777757e7d43b5763b7766ae85e3a8eb1e26
Harald Fielker <harald.fielker@gmail.com>
2023-06-13 06:30:42 +0200

    fixed

--- packer/pack.sh
+++ packer/pack.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
 
-mkdir -p /tmp/amd64/usr/zen/include
-mkdir -p /tmp/amd64/usr/zen/lib
+mkdir -p /tmp/amd64/usr/include
+mkdir -p /tmp/amd64/usr/lib
 
-cp /workdir/bindings/ffi/bindings.h /tmp/amd64/usr/zen/include
-cp /workdir/target/release/libzen_ffi.a /tmp/amd64/usr/zen/lib
+cp /workdir/bindings/ffi/bindings.h /tmp/amd64/usr/include
+cp /workdir/target/release/libzen_ffi.a /tmp/amd64/usr/lib
 
-# get from Cargo.toml
-VERSION="1.10.2"
+# get from Cargo.toml (https://github.com/rust-lang/cargo/issues/4082)
+VERSION="0.0.1"
 
 archs="amd64"
 targets="deb rpm"
 name="zen"
 
+# https://www.digitalocean.com/community/tutorials/how-to-use-fpm-to-easily-create-packages-in-multiple-formats
+
+# fpm -s dir -t deb -C /tmp/project --name project_name --version 1.0.0 --iteration 1
+# --depends debian_dependency1  .
+
+
 for arch in $archs; do
     for target in $targets; do
-        cd /tmp/${arch}
-        fpm  -s dir -t ${target} -v ${VERSION} -n ${name}  \
-            -a ${arch} ./usr/zen/include ./usr/zen/lib
+        fpm -s dir -t ${target} -v ${VERSION} -n ${name}  \
+           --description "GoRules.io ZEN bindings" \
+           -a ${arch} -f -C /tmp/${arch} -p /workdir/target/release
     done
 done
 
-dpkg --contents /tmp/amd64/${name}_${VERSION}_amd64.deb
+dpkg --contents /workdir/target/release/${name}_${VERSION}_amd64.deb
+
+# sudo apt-get install ./zen_0.0.1_amd64.deb
+# sudo apt-get remove zen
+
 
 # https://fpm.readthedocs.io/en/v1.15.1/packages/osxpkg.html
 # mac - set target to osxpkg (ensure what paths you need)
